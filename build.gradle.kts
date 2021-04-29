3866ae80e53cc4f9953702a5e2be01707361c2b7
Werner Froidevaux <wfro64@users.noreply.github.com>
2021-04-29 20:40:06 +0200

    Fixes #83

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ plugins {
 
 allprojects {
     group = "org.opencrx"
-    version = "5.1-20210428"
+    version = "5.1-20210429"
 }
 
 java {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -55,7 +55,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("org.opencrx:opencrx-gradle:5.1-20210428")
+        classpath("org.opencrx:opencrx-gradle:5.1-20210429")
     }
 }
 rootProject.name = "opencrx"
